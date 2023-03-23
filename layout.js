d13fd84a97910ec4c9a8587a8b775542e1483b0b
Misha <mrejzlin@gmail.com>
2023-03-23 01:26:34 +0100

    better default styles

--- layout.js
+++ layout.js
@@ -5,7 +5,9 @@ export function layout(content) {
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- <link rel="stylesheet" href="//writ.cmcenroe.me/1.0.4/writ.min.css"> -->
 <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/yegor256/tacit@gh-pages/tacit-css-1.5.5.min.css"/> -->
-<link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
+<!-- <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css"> -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+
 </head>
 <body>
   ${content}
